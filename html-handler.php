a19f025b7a1c14b51e2c3f2a0ff4163f15e29573
Sean Hogan <shogun70@gmail.com>
2018-08-25 13:12:06 +1000

    The path to the boot-script was incorrect in sub-directories.

--- html-handler.php
+++ html-handler.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
 <meta charset="UTF-8" />
-<script src="./boot.js"></script>
+<script src="<?= preg_replace('/[^\/]+$/', 'boot.js', preg_replace('/[^\/]+\//', '../', $_GET['path'])) ?>"></script>
 <plaintext type="text/markdown">
 <?php include($_GET['path']); ?>
